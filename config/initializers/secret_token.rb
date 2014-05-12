f095db0caf41507ba0ea26331797fd756c35f8f7
f-l-a-m <derlowe18@gmail.com>
2014-05-12 20:18:36 +0300

    Created rails app, some css, user model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QandA::Application.config.secret_key_base = 'd79c5d65debe3be19a2fca5149679865ca684cfb239dd3dd5d1778127421d0b4a8980d0392800ff866e2c6f3eed1117f4a2a88394cddfc7ba49da863f6f5674b'
